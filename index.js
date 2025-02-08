fd6033d4b2ed9099f348166a429e4749334e368e
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-08 19:28:04 +0800

    change latter case

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var letter = prompt("Enter a letter : ");
+
+letter= letter.toLocaleLowerCase();
+
 if (letter=="a"||letter=="e"||letter=="i"||letter=="o"||letter=="u")
     document.write("vowel");
+
 else
     document.write("consonat");
